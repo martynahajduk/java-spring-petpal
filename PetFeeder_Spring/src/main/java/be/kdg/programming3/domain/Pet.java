eec535a6dd39bab1d6c062122966b74ee785be52
alina.dimova <alina.dimova@student.kdg.be>
2024-10-14 14:53:52 +0200

    adding id attribute

--- PetFeeder_Spring/src/main/java/be/kdg/programming3/domain/Pet.java
+++ PetFeeder_Spring/src/main/java/be/kdg/programming3/domain/Pet.java
@@ -1,13 +1,32 @@
 package be.kdg.programming3.domain;
 
-public class Pet {
-    String name;
-    int age;
-    String gender;
-    Breed animalType;
-    double weight;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-    public Pet(String name, int age, Breed animalType, String gender, double weight) {
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int age;
+    private String gender;
+    private Breed animalType;
+    private double weight;
+
+
+    protected Pet() {
+    } //Encapsulation: You don’t want other parts of the application
+    // (except for subclasses) to use the default constructor. By marking it as
+    // protected, only the JPA framework
+    // (and subclasses of Pet) can use it
+
+
+
+    public Pet(Long id, String name, int age, Breed animalType, String gender, double weight) {
+        this.id = id;
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -17,6 +36,13 @@ public class Pet {
 
     public String getName() {
         return name;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {
@@ -55,7 +81,18 @@ public class Pet {
         this.weight = weight;
     }
 
+    @Override
     public String toString() {
-        return name + " " + age + " " + gender + " " + animalType + " " + weight;
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", animalType=" + animalType +
+                ", gender='" + gender + '\'' +
+                ", weight=" + weight +
+                '}';
     }
 }
+
+
+
